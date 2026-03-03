c5339bba76e15be270b9d87db9259ab2e2a40258
joshua <joshuagarcia1600@gmail.com>
2026-03-02 17:00:14 -0800

    Guarda los cambios que se agregaron en la carpeta OOP

--- OOP/Enemigo.py
+++ OOP/Enemigo.py
@@ -3,7 +3,7 @@ class Enemigo:
     puntos_energia: int = 10
     ataque = 1
 
-    def __int__(self, tipo_enemigo, puntos_energia=10, ataque=1):
+    def __init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
         self.__tipo_enemigo = tipo_enemigo
         self.puntos_energia = puntos_energia
         self.ataque = ataque
@@ -12,7 +12,13 @@ class Enemigo:
         return self.__tipo_enemigo
     
     def habla(self):
-        print(f"Yo soy {self.__tipo_enemigo}. Preparando para pelear!!")
+        print(f"Yo son {self.__tipo_enemigo}. Preparado para palear!!")
 
     def camina(self):
-        print(f"{self.__tipo_enemigo} se mueve cerca de ti!!!")
+        print(f"{self.__tipo_enemigo} se mueve cerca de ti!!")
+
+    def atacar(self):
+        print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño!!")
+
+    def ataque_especial(self):
+        print("Enemigo no tiene ataque especial")
